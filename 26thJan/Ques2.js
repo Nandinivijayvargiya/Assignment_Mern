1da1725d9727284527653867f71d1d9ec1d86c37
Nandinivijayvargiya <48726692+Nandinivijayvargiya@users.noreply.github.com>
2022-01-27 08:38:51 +0530

    Add files via upload

---
+++ 26thJan/Ques2.js
@@ -0,0 +1,21 @@
+//
+
+var a=40;
+var b=80;
+var c=100;
+
+if(a>b && a>c)
+{
+	console.log(a+" "+"is the greatest among three numbers");
+}
+else if(b>a && b>c)
+{
+	console.log(b+" "+"is greatest among three numbers");
+}
+else
+{
+	console.log(c+" "+"is greatest among all three numbers");
+	
+}
+
+
